52ccdd7183d5eeb79e773c4a53145304abeca5d4
지민재 <wigus02@naver.com>
2023-11-27 16:20:08 +0900

    change code

--- src/main/java/com/example/Volunteer/repository/InfoRepository.java
+++ src/main/java/com/example/Volunteer/repository/InfoRepository.java
@@ -11,17 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface InfoRepository extends JpaRepository<Info,Long> {
-    @Modifying
-    @Query("UPDATE Info i SET i.total_hours = i.total_hours + :hours WHERE i.user.id = :userId")
-    void updateTotalHours(@Param("userId") Long userId, @Param("hours") int hours);
 
-    @Modifying
-    @Query("UPDATE Info i SET i.grade = CASE " +
-            "WHEN i.total_hours < 50 THEN '초보자' " +
-            "WHEN i.total_hours < 100 THEN '중급자' " +
-            "ELSE '경력자' END " +
-            "WHERE i.user.id = :userId")
-    void updateGrade(@Param("userId") Long userId);
     Info findByUser(User user);
 }
 
