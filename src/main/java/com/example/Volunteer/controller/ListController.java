bfaff655a7caa8669fb5b55c4cf4dc588ddc2a50
지민재 <wigus02@naver.com>
2023-11-28 17:13:32 +0900

    add update volunteerList code

--- src/main/java/com/example/Volunteer/controller/ListController.java
+++ src/main/java/com/example/Volunteer/controller/ListController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,8 +25,21 @@ public class ListController {
     private List<VolunteerList> volunteerList = new ArrayList<>();
     @GetMapping("/getVolunteerList")
     public List<VolunteerList> getVolunteerList() {
-        // 여기서는 간단한 더미 데이터를 반환하도록 하겠습니다.
-        return volunteerListRepository.findAll(); // 모든 데이터 가져오기
+        // VolunteerListRepository에서 모든 데이터를 가져오기
+        List<VolunteerList> allVolunteerList = volunteerListRepository.findAll();
+
+        // 현재 날짜를 가져오는 코드 (Java 8 이상에서 제공)
+        LocalDate currentDate = LocalDate.now();
+
+        // 마감일이 지나지 않은 봉사 리스트 필터링
+        List<VolunteerList> openVolunteerList = new ArrayList<>();
+        for (VolunteerList volunteer : allVolunteerList) {
+            LocalDate endDate = volunteer.getVolunteerForm().getEnd_date();
+            if (endDate != null && endDate.isAfter(currentDate)) {
+                openVolunteerList.add(volunteer);
+            }
+        }
+        return openVolunteerList;
     }
     // 기타 필요한 엔드포인트 및 메서드 추가 가능
 }
